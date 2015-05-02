4e7fe3c06bf2bec3b403099a488cc7f56e1a0815
srz_zumix <zumix.cpp@gmail.com>
2015-05-02 17:09:16 +0900

    twilio test

--- test/test.cpp
+++ test/test.cpp
@@ -5,6 +5,11 @@ IUTEST_TEST(Test, A)
 	IUTEST_ASSERT_EQ(0, 0);
 }
 
+IUTEST_TEST(DISABLED_Test, Fail)
+{
+	IUTEST_ASSERT_EQ(0, 1);
+}
+
 int main(int argc, char* argv[])
 {
 	IUTEST_INIT(&argc, argv);
